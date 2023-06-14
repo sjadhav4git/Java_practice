0192cfad331cbf983338df4e54ed7ca10be2a1da
sjadhav4git <sjadhav4@buffalo.edu>
2023-06-14 14:44:47 -0400

    Odd_numbers.java

---
+++ odd_numbers.java
@@ -0,0 +1,10 @@
+public class odd_numbers {
+    public static void main(String[] args) {
+        System.out.println("Odd numbers");
+        for(byte i=0; i<10; i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+    }
+}
