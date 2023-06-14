777063b714ca4b35c2a13a19a7ea71442dec8391
sjadhav4git <sjadhav4@buffalo.edu>
2023-06-14 14:41:33 -0400

    even_numbers.java

---
+++ even_numbers.java
@@ -0,0 +1,10 @@
+public class even_numbers {
+    public static void main(String[] args) {
+        System.out.println("even numbers");
+        for (int i=0;i<10;i++){
+            if (i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
