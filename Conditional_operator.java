bbfabe5c8e9cb27fb69ae6ff0839e07c1475cfe9
sjadhav4git <sjadhav4@buffalo.edu>
2023-06-22 00:33:21 -0400

    Turnery Operator

---
+++ Conditional_operator.java
@@ -0,0 +1,8 @@
+public class Conditional_operator {
+    public static void main(String[] args) {
+        char c = 'a';
+        char d = 'b';
+        char result = (c>d)?c:d;
+        System.out.println(result);
+    }
+}
