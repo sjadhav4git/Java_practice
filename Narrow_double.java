35e12beb5aa59d154d3358861352a14f95233cae
sjadhav4git <sjadhav4@buffalo.edu>
2023-06-22 00:24:15 -0400

    Narrow_double to all types

---
+++ Narrow_double.java
@@ -0,0 +1,23 @@
+public class Narrow_double {
+    public static void main(String[] args) {
+        double d = 20.555;
+        float f = (float) d;
+        System.out.println(f);
+
+        long l = (long) d;
+        System.out.println(l);
+        
+        int i = (int) d;
+        System.out.println(i);
+        
+        char c = (char) d;
+        System.out.println(c);
+        
+        short s = (short) d;
+        System.out.println(s);
+
+        byte b = (byte) d;
+        System.out.println(b);
+
+    }
+}
