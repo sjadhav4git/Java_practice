d613811090508170f5425108bcf91165a766acf6
sjadhav4git <sjadhav4@buffalo.edu>
2023-06-29 14:10:30 -0400

    Char_to_ascii

---
+++ ChartoAscii.java
@@ -0,0 +1,9 @@
+public class ChartoAscii {
+    public static void main(String[] args) {
+        System.out.println(charToAscii('a'));
+    }
+
+    public static int charToAscii(char c){
+        return c;
+    }
+}
