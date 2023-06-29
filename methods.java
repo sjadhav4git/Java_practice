b5d8af43964985d42aca16d6f9c696d7e8e0c9fc
sjadhav4git <sjadhav4@buffalo.edu>
2023-06-29 13:16:01 -0400

    method_basics

---
+++ methods.java
@@ -0,0 +1,32 @@
+public class methods {
+    public static void main(String[] args) {
+        System.out.println(add(5, 6));
+    }
+
+    public static int add(int a, int b){
+        return (a+b);
+    }
+}
+
+
+/*
+* method types:
+* 1) parameterized method
+* 2) non-parameterized method
+
+ * access modifier - public, default, protected, private
+ * 1) public - all packages, all classes
+ * 2) private - only class
+ * 3) default - its own package
+ * 4) protected - its package and other packages subclass
+ *
+ * non access modifier
+ * 1) static
+ * 2) final
+ * 3) abstract
+ * 
+ * 
+ * return type:
+ * 1) void
+ * 2) primitive data type
+ */
